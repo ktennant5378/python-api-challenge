a92dfb45aadf8b1282e088b514a6ce0f35d32e55
Kenway Tennant <kengtennant@gmail.com>
2024-02-08 12:57:32 -0700

    Updated api key

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "5f0328b670387ae35e46fd1014f22d30"
 
 # Geoapify API Key
-geoapify_key = ""
+geoapify_key = "9fe79ea7ee434908b552995989da3ae6"
