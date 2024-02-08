067d61ad10b956fcad4bc2d2c703e8ca10af55f9
Kenway Tennant <kengtennant@gmail.com>
2024-02-08 11:52:23 -0700

    - Changed API Key for testing

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "91320161202f8a575439f0bd638a58c3"
+weather_api_key = "5f0328b670387ae35e46fd1014f22d30"
 
 # Geoapify API Key
 geoapify_key = "9fe79ea7ee434908b552995989da3ae6"
